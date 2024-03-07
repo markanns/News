728296e2a45383bf7feb913f29c8d61e70272a6c
Marko Kostresevic <kostres90@hotmail.com>
2024-03-07 14:09:17 +0100

    Add files via upload

--- news/src/components/NewsContext/NewsContext.tsx
+++ news/src/components/NewsContext/NewsContext.tsx
@@ -2,16 +2,16 @@ import {
   ReactNode,
   createContext,
   useState,
-  useEffect,
   useContext,
 } from "react";
+
+import useTopNews from "../../hooks/useTopNews";
+
 
 type NewsContextType = {
   country: string;
   isActive: boolean;
   news: NewsItem[];
-  businessNews: NewsItem[];
-  technologyNews: NewsItem[];
   handleClickOnCountryButton: (
     event: React.MouseEvent<HTMLButtonElement>
   ) => void;
@@ -22,8 +22,6 @@ const NewsContext = createContext<NewsContextType>({
   country: "",
   news: [],
   isActive: true,
-  businessNews: [],
-  technologyNews: [],
   handleClickOnCountryButton: () => {},
   handleButtonsState: () => {},
   handleActiveButton: () => {},
@@ -41,39 +39,10 @@ export const useNewsContext = () => {
 };
 
 export const NewsProvider = ({ children }: { children: ReactNode }) => {
-  const [news, setNews] = useState<NewsItem[]>([]);
   const [country, setCountry] = useState<string>("US");
   const [isActive, setIsActive] = useState<boolean>(true);
-  const [businessNews, setBusinessNews] = useState<NewsItem[]>([]);
-  const [technologyNews, setTechnologyNews] = useState<NewsItem[]>([]);
+  const {news} = useTopNews(country);
 
-  useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=f62196a68b7b41d385329a19658c8625`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setBusinessNews(data.articles);
-      });
-  }, [country]);
-  useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=${country}&category=technology&apiKey=f62196a68b7b41d385329a19658c8625`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setTechnologyNews(data.articles);
-      });
-  }, [country]);
-  useEffect(() => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=f62196a68b7b41d385329a19658c8625`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setNews(data.articles);
-      });
-  }, [country]);
   const handleClickOnCountryButton = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -93,8 +62,6 @@ export const NewsProvider = ({ children }: { children: ReactNode }) => {
     <NewsContext.Provider
       value={{
         country,
-        businessNews,
-        technologyNews,
         handleClickOnCountryButton,
         news,
         handleButtonsState,
