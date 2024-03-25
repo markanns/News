cebe159bc3b8a9fc766293954571c9ad14640ccf
Marko Kostresevic <kostres90@hotmail.com>
2024-03-25 09:37:02 +0100

    Add files via upload

--- news/src/components/TopNews/TopNews.tsx
+++ news/src/components/TopNews/TopNews.tsx
@@ -2,20 +2,14 @@ import Thumbnail from "../Thumbnail/Thumbnail";
 import { NewsHolder } from "./StyledTopNews";
 import { Wrap } from "../../styles/Global";
 import { useNewsContext } from "../../context/NewsContext";
-import { NewsItem } from "../../types/Article";
 import useNews, { GetTopNews } from "../../hooks/useNews";
 
 const TopNews = () => {
   const { country } = useNewsContext();
-  const { news, isLoading, isError } = useNews(
-    GetTopNews as (arg1: string, arg2?: string | undefined) => Promise<{ data: NewsItem[]; error: undefined }>,
-    country,
-    ""
-  );
-
+  const { data: news, isPending, isError } = useNews(GetTopNews, country);
   const newsList = (
     <>
-      {news.map((item, index) => (
+      {news?.map((item, index) => (
         <Thumbnail
           key={index}
           title={item.title}
@@ -26,13 +20,12 @@ const TopNews = () => {
       ))}
     </>
   );
-
   return (
     <Wrap>
       <h2>Top news from {country}</h2>
       <NewsHolder>
         {isError && <p>Something went wrong...</p>}
-        {isLoading && <p>Loading...</p>}
+        {isPending && <p>Loading...</p>}
         {newsList}
       </NewsHolder>
     </Wrap>
